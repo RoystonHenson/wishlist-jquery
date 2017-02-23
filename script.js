2a0c76c20eb2feeb4f8827bd1f3fc76187fca8d2
Royston Henson <rlqhenson@hotmail.com>
2017-02-23 10:33:19 +0000

    remove pending label on click, add completed label on click

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ function addToList(item) {
     $('#items').append('<li>' + item + '<span class=\'label pending\'>Pending</span></li>');
   }
 
-$(document).on('click', addToList, function() {
+$(document).on('click', '#add-to-list', function() {
   addToList($('#item').val());
   $('#item').focus();
+});
+
+$(document).on('click', '.pending', function() {
+
+  $(this).parent().append('<span class=\'label success\'>Completed!</span>');
+  $('.pending').remove();
 });
