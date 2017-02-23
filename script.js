b3ef7491b9831c8d10c99fc8c1ea56cadf68f6b9
Royston Henson <rlqhenson@hotmail.com>
2017-02-23 11:07:50 +0000

    add total item status updates

--- script.js
+++ script.js
@@ -2,12 +2,20 @@ function addToList(item) {
     $('#items').append('<li>' + item + '<span class=\'label pending\'>Pending</span></li>');
   }
 
+function updateTotal() {
+  var pending = $('.pending').length;
+  var completed = $('.completed').length;
+  $('#total').text('Completed: ' + completed + ' | ' + 'Pending: ' + pending);
+}
+
 $(document).on('click', '#add-to-list', function() {
   addToList($('#item').val());
   $('#item').focus();
+  updateTotal();
 });
 
 $(document).on('click', '.pending', function() {
   $(this).parent().addClass('completed');
   $(this).remove();
+  updateTotal();
 });
